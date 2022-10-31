fa999344bd960721613eba99dba1f543e22c2c84
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-10-31 20:35:19 +0100

    fix: toggle styles for completed tasks

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ var taskCompleted = function(){
 
     //Append the task list item to the #completed-tasks
     var listItem = this.parentNode;
+    listItem.querySelector(".task-item-label").classList.add("completed-tasks-label");
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
 
@@ -135,6 +136,7 @@ var taskIncomplete = function(){
     //When the checkbox is unchecked
     //Append the task list item to the #incompleteTasks.
     var listItem = this.parentNode;
+    listItem.querySelector(".task-item-label").classList.remove("completed-tasks-label");
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskCompleted);
 }
