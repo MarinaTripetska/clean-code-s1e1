dccf9184f98d92bc42abd8fd3c0f463571148773
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-10-31 17:59:25 +0100

    refactor: change selectors, remove tag name from css according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var completedTasksHolder=document.querySelector(".completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.className = "task-item";
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
@@ -33,18 +33,20 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task task-item-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="task-item-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task-item-input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
